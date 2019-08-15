4f4ac32e5152c5eb5aec64ec6c6f4cbe20e3b3b6
KseniyaCo <kseniya27392@gmail.com>
2019-08-15 17:42:56 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import marked from 'marked'
 
-const URL = "https://habrastorage.org/webt/3f/lq/sf/3flqsfiqzb6yrzksg0o76xiagg0.png";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      result: `![image example](${URL})\n-->`
+      result: `# Marked in the browser\nRendered by **[marked.js.org](https://marked.js.org/)**.`
     }
   }
 
@@ -28,14 +27,14 @@ class App extends React.Component {
         <div className="textareas column">
           <div className="textareaName">
             <p className="paragraphName">Editor</p>
-            <textarea onChange={this.handleChange} 
+            <textarea  id="editor" onChange={this.handleChange} 
             placeholder="Welcome to React Markdown Previewer!" 
             cols="60" 
             rows="10"></textarea>
           </div>
           <div className="textareaName">
             <p className="paragraphName">Previewer</p>
-            <div className="output" dangerouslySetInnerHTML={this.getMarkedText()}/>
+            <div id="preview" className="output" dangerouslySetInnerHTML={this.getMarkedText()}/>
           </div>
         </div>
       </div>
